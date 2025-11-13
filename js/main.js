41cdb407efd659043d63f6927f43ee839dae51ff
VanJichook <alsdntjr78@naver.com>
2025-11-14 04:16:52 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,39 +64,48 @@ function loadBandsintownWidget() {
   const init = document.createElement("a");
   init.className = "bit-widget-initializer";
 
-  // ---- 전체 스타일 옵션 ----
-  init.setAttribute("data-artist-name", "id_15583383");
+   init.setAttribute("data-artist-name", "id_15583383");
+  init.setAttribute("data-events-to-display", "");
   init.setAttribute("data-background-color", "rgba(255,255,255,1)");
   init.setAttribute("data-separator-color", "rgba(115,207,217,1)");
   init.setAttribute("data-text-color", "rgba(34,36,38,1)");
   init.setAttribute("data-font", "Andalé Mono");
   init.setAttribute("data-auto-style", "true");
 
-  // Capitalization
   init.setAttribute("data-button-label-capitalization", "uppercase");
   init.setAttribute("data-header-capitalization", "uppercase");
   init.setAttribute("data-location-capitalization", "uppercase");
   init.setAttribute("data-venue-capitalization", "uppercase");
-
-  // Dates
   init.setAttribute("data-display-local-dates", "true");
   init.setAttribute("data-local-dates-position", "tab");
   init.setAttribute("data-display-past-dates", "true");
+  init.setAttribute("data-display-details", "false");
+  init.setAttribute("data-display-lineup", "false");
+  init.setAttribute("data-display-start-time", "true");
+  init.setAttribute("data-social-share-icon", "false");
+  init.setAttribute("data-display-limit", "all");
+
   init.setAttribute("data-date-format", "MMM. D, YYYY");
   init.setAttribute("data-date-orientation", "horizontal");
   init.setAttribute("data-date-border-color", "#4A4A4A");
   init.setAttribute("data-date-border-width", "1px");
+  init.setAttribute("data-date-capitalization", "capitalize");
   init.setAttribute("data-date-border-radius", "10px");
 
-  // Ticket Button
+  init.setAttribute("data-event-ticket-cta-size", "medium");
+  init.setAttribute("data-event-custom-ticket-text", "");
   init.setAttribute("data-event-ticket-text", "TICKETS");
+  init.setAttribute("data-event-ticket-icon", "false");
   init.setAttribute("data-event-ticket-cta-text-color", "rgba(255,255,255,1)");
   init.setAttribute("data-event-ticket-cta-bg-color", "rgba(115,207,217,1)");
   init.setAttribute("data-event-ticket-cta-border-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-event-ticket-cta-border-width", "0px");
+  init.setAttribute("data-event-ticket-cta-border-radius", "2px");
 
-  // Follow Button
-  init.setAttribute("data-follow-section-cta-bg-color", "rgba(115,207,217,1)");
-  init.setAttribute("data-follow-section-cta-text-color", "rgba(255,255,255,1)");
+  init.setAttribute("data-sold-out-button-text-color", "rgba(255,255,255,1)");
+  init.setAttribute("data-sold-out-button-background-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-sold-out-button-border-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-sold-out-button-clickable", "true");
 
   init.setAttribute("data-event-rsvp-position", "left");
   init.setAttribute("data-event-rsvp-cta-size", "medium");
@@ -108,6 +117,35 @@ function loadBandsintownWidget() {
   init.setAttribute("data-event-rsvp-cta-border-color", "rgba(115,207,217,1)");
   init.setAttribute("data-event-rsvp-cta-border-width", "1px");
   init.setAttribute("data-event-rsvp-cta-border-radius", "2px");
+
+  init.setAttribute("data-follow-section-position", "top");
+  init.setAttribute("data-follow-section-alignment", "center");
+  init.setAttribute("data-follow-section-header-text", "Get updates on new shows, new music, and more");
+  init.setAttribute("data-follow-section-cta-size", "medium");
+  init.setAttribute("data-follow-section-cta-text", "FOLLOW");
+  init.setAttribute("data-follow-section-cta-icon", "false");
+  init.setAttribute("data-follow-section-cta-text-color", "rgba(255,255,255,1)");
+  init.setAttribute("data-follow-section-cta-bg-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-follow-section-cta-border-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-follow-section-cta-border-width", "0px");
+  init.setAttribute("data-follow-section-cta-border-radius", "2px");
+
+  init.setAttribute("data-play-my-city-position", "bottom");
+  init.setAttribute("data-play-my-city-alignment", "center");
+  init.setAttribute("data-play-my-city-header-text", "Don’t see a show near you?");
+  init.setAttribute("data-play-my-city-cta-size", "medium");
+  init.setAttribute("data-play-my-city-cta-text", "REQUEST A SHOW");
+  init.setAttribute("data-play-my-city-cta-icon", "false");
+  init.setAttribute("data-play-my-city-cta-text-color", "rgba(255,255,255,1)");
+  init.setAttribute("data-play-my-city-cta-bg-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-play-my-city-cta-border-color", "rgba(115,207,217,1)");
+  init.setAttribute("data-play-my-city-cta-border-width", "0px");
+  init.setAttribute("data-play-my-city-cta-border-radius", "2px");
+
+  init.setAttribute("data-language", "en");
+  init.setAttribute("data-layout-breakpoint", "900");
+  init.setAttribute("data-bit-logo-position", "bottomRight");
+  init.setAttribute("data-bit-logo-color", "rgba(34,36,38,1)");
 
   container.appendChild(init);
 
